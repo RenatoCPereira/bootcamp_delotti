f41077a281e22d6624a0ed2c128291b14b9d9f11
RenatoCPereira <renatocarlos8960@gmail.com>
2026-03-10 21:29:20 -0300

    Ajuste do pom.xml e add swagger

---
+++ src/main/java/com/example/bootcamp_delotti/BootcampDelottiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp_delotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDelottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDelottiApplication.class, args);
+	}
+
+}
